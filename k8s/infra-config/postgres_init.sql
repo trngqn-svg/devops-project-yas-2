3070ecdf524134a15e06c6908640d13fde542e96
Tran Trong Quan <trantrongquan062@gmail.com>
2026-06-22 22:25:19 +0700

    feat: add umbrella chart

---
+++ k8s/infra-config/postgres_init.sql
@@ -0,0 +1,14 @@
+SELECT 'Khởi tạo danh sách các database cho hệ thống YAS...';
+
+CREATE DATABASE keycloak;
+CREATE DATABASE media;
+CREATE DATABASE product;
+CREATE DATABASE cart;
+CREATE DATABASE rating;
+CREATE DATABASE "order";
+CREATE DATABASE location;
+CREATE DATABASE inventory;
+CREATE DATABASE promotion;
+CREATE DATABASE tax;
+CREATE DATABASE customer;
+CREATE DATABASE payment;
